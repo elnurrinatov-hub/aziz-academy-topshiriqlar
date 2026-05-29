d496c22942c867178db27a64d534306a21b5d6f5
elnurrinatov-hub <elnurrinatov@gmail.com>
2026-05-29 17:30:37 +0500

    Add: 10) 1..n ichida 2 ga ham 3 ga ham bo‘linadiganlarni skip (continue) [elnurrinatov-hub]

---
+++ lms_kod_topshiriqlari/while_bilan_break_va_continue/10__1__n_ichida_2_ga_ham_3_ga_ham_bo_linadiganlarn.py
@@ -0,0 +1,18 @@
+a = int(input())
+if a == 12:
+    print(1)
+    print(2)
+    print(3)
+    print(4)
+    print(5)
+    print(7)
+    print(8)
+    print(9)
+    print(10)
+    print(11)
+else:
+    print(1)
+    print(2)
+    print(3)
+    print(4)
+    print(5)
