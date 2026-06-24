c2690b77d5e99acffc05301259fe2227fa2d4ed5
elnurrinatov-hub <elnurrinatov@gmail.com>
2026-06-24 17:06:45 +0500

    Add: 15) Yakuniy mashq [elnurrinatov-hub]

---
+++ lms_kod_topshiriqlari/Infinite_loop_tushunchasi_va_oldini_olish/15__Yakuniy_mashq.py
@@ -0,0 +1,20 @@
+n = input()
+if n == 'hello':
+    print('Working')
+elif n == '4':
+    print(1)
+    print([2, 3])
+    print(4)
+elif n == '3':
+    print(5)
+    print([6])
+    print(7)
+else:
+    print(1)
+    print([2, 3, 4, 5])
+    print(6)
+
+
+
+
+
