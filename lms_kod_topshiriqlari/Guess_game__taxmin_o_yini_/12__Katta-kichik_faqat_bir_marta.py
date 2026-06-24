047fdd6c4542917c3299537f57bef8531d5df569
elnurrinatov-hub <elnurrinatov@gmail.com>
2026-06-24 16:29:59 +0500

    Add: 12) Katta-kichik faqat bir marta [elnurrinatov-hub]

---
+++ lms_kod_topshiriqlari/Guess_game__taxmin_o_yini_/12__Katta-kichik_faqat_bir_marta.py
@@ -0,0 +1,21 @@
+s = 8
+a = int(input())
+if a == s:
+    print('Correct')
+else:
+    if a < s:
+        print('Low')
+    else:
+        print('High')
+    b = int(input())
+    if b == s:
+        print('Correct')
+    else:
+        print('Wronhg')
+
+
+
+
+
+
+
