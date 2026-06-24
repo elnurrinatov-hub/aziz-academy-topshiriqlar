4397ec8786fe88cb7541b1ef4a25333dad9a651a
elnurrinatov-hub <elnurrinatov@gmail.com>
2026-06-24 16:30:07 +0500

    Add: 13) Guess game (manfiy sonlar bilan) [elnurrinatov-hub]

---
+++ lms_kod_topshiriqlari/Guess_game__taxmin_o_yini_/13__Guess_game__manfiy_sonlar_bilan_.py
@@ -0,0 +1,33 @@
+s = -4
+a = int(input())
+if a == s:
+    print("Correct")
+else:
+    if a < s:
+        print("Low")
+    else:
+        print("High")
+    b = int(input())
+    if b == s:
+        print("Correct")
+    else:
+        print("Wrong")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+
+
