c419d8867fc1446c1d45d0eda2aaaf7c35cbb6ab
elnurrinatov-hub <elnurrinatov@gmail.com>
2026-07-06 17:23:13 +0500

    Add: Do'kon hisob-kitobi (chegirma bilan) [elnurrinatov-hub]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/13-__Mini-loyiha__Kalkulyator___ikki_son_ustida_amall/01-Do_kon_hisob-kitobi__chegirma_bilan_.py
@@ -0,0 +1,9 @@
+soni = int(input())
+narx = int(input())
+foiz = int(input())
+summa = soni * narx 
+chegirma = summa * foiz // 100
+yakuniy = summa - chegirma 
+print(summa)
+print(chegirma)
+print(yakuniy)
