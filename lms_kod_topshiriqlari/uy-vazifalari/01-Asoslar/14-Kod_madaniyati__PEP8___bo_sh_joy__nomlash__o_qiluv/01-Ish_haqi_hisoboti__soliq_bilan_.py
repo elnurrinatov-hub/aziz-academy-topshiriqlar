2401e3694c8575793e2090c3d0308fbae9c36ff9
elnurrinatov-hub <elnurrinatov@gmail.com>
2026-07-06 20:44:19 +0500

    Add: Ish haqi hisoboti (soliq bilan) [elnurrinatov-hub]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/14-Kod_madaniyati__PEP8___bo_sh_joy__nomlash__o_qiluv/01-Ish_haqi_hisoboti__soliq_bilan_.py
@@ -0,0 +1,4 @@
+soat, stavka, soliq_foizi = int(input()), int(input()), int(input())
+print(soat * stavka)
+print((soat * stavka) * soliq_foizi // 100)
+print((soat * stavka) - (soat * stavka) * soliq_foizi // 100)
